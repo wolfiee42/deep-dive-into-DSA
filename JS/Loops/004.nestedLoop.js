06c78513f8fe3c8e1a289a266a9dc043ad86c0d5
wolfiee42 <saifalislam2022@gmail.com>
2024-04-10 16:41:43 +0600

    did some problem solving with nested loop

---
+++ JS/Loops/004.nestedLoop.js
@@ -0,0 +1,35 @@
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
+
+// ----------------------- solution
+
+let m = 5;
+
+for (let i = 1; i <= m; i++) {
+  let result = "";
+  for (let j = 1; j <= i; j++) {
+    result += j + " ";
+  }
+  console.log(result);
+}
+
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+
+// ----------------------- solution
+
+let n = 5;
+
+for (let i = 1; i <= n; i++) {
+  let result = "";
+  for (let j = 1; j <= n; j++) {
+    result += "*" + " ";
+  }
+  console.log(result);
+}
