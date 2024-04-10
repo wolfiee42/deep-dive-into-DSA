80d92f076e5223ec79319175a792bddef1564699
wolfiee42 <saifalislam2022@gmail.com>
2024-04-10 17:47:24 +0600

    understanding the concept of nesting loop more deeply by solving problem

--- JS/Loops/004.nestedLoop.js
+++ JS/Loops/004.nestedLoop.js
@@ -33,3 +33,21 @@ for (let i = 1; i <= n; i++) {
   }
   console.log(result);
 }
+
+// 1 2 3 4 5
+// 1 2 3 4
+// 1 2 3
+// 1 2
+// 1
+
+// ----------------------- solution
+
+let k = 5;
+
+for (let i = k; i >= 1; i--) {
+  let result = "";
+  for (let j = 1; j <= i; j++) {
+    result += j + " ";
+  }
+  console.log(result);
+}
