31c5c7c1ccb55443a6a1651481a494b85e888c5c
wolfiee42 <saifalislam2022@gmail.com>
2024-04-14 20:06:53 +0600

    understanding how mulridymansional array works

---
+++ JS/002.Array/004.multidymensionalArr.js
@@ -0,0 +1,11 @@
+const multiDymensionalArr = [
+  [12, 23, 34, 45],
+  [65, 54, 43, 32],
+  [76, 78, 98, 55],
+];
+
+for (let i = 0; i < multiDymensionalArr.length; i++) {
+  for (let j = 0; j < multiDymensionalArr[i].length; j++) {
+    console.log(multiDymensionalArr[i][j]);
+  }
+}
