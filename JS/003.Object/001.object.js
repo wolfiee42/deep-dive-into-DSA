dd089a4c4a32254c92d00d01c9f3916d89b02cdb
wolfiee42 <saifalislam2022@gmail.com>
2024-04-16 06:29:32 +0600

    understanding the basic of object in js, even though i know it.

---
+++ JS/003.Object/001.object.js
@@ -0,0 +1,28 @@
+// Object in Javascript
+
+let object01 = {};
+object01.a = 10;
+object01.b = 20;
+
+console.log(object01);
+
+let object02 = {
+  x: 22,
+  y: 23,
+};
+
+object02.z = 24;
+
+console.log(object02);
+
+let object03 = Object();
+object03.k = "gracious";
+object03.l = "hola";
+
+console.log(object03);
+
+let object04 = new Object();
+object04.p = true;
+object04.q = false;
+
+console.log(object04);
