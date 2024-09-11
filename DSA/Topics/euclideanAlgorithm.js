21fba1855ecdc227bb5f02eda49f08e0e3e973cb
wolfiee42 <saifalislam2022@gmail.com>
2024-09-11 22:56:25 +0600

    finding greatest common divisor using Euclidean Algorithms but without recursion and basic logic

--- DSA/Topics/euclideanAlgorithm.js
+++ DSA/Topics/euclideanAlgorithm.js
@@ -1,3 +1,18 @@
+// Basic
+function gcd(a, b) {
+  if (a == b) return a;
+  if (a > b) {
+    return gcd(a - b, b);
+  } else {
+    return gcd(a, b - a);
+  }
+}
+
+console.log(gcd(10, 15)); // 5
+console.log(gcd(35, 10)); // 5
+console.log(gcd(31, 2)); // 1
+
+// using recursion
 function euclideanAlgorithms(a, b) {
   if (a === 0) return b;
   return euclideanAlgorithms(b % a, a);
