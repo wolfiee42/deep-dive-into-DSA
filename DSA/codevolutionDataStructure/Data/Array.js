b058074db9f96f0b72117945ec45d837395a60d5
wolfiee42 <saifalislam2022@gmail.com>
2024-09-12 17:07:41 +0600

    // writing methods without actually writing methods using raw js

---
+++ DSA/codevolutionDataStructure/Data/Array.js
@@ -0,0 +1,32 @@
+let arr = [1, 2, 3, "Saif"];
+
+// writing methods without actually writing methods using raw js
+
+// arr.push(4);
+// const lastIndex = arr.length;
+// arr[lastIndex] = 4;
+
+// arr.unshift(0);
+// let newElement = 0;
+// let newArr = [newElement];
+// for (let i = 0; i < arr.length; i++) {
+//   newArr[i + 1] = arr[i];
+// }
+
+// arr.pop();
+// const lastIndex = arr.length - 1;
+// let newArr = [];
+// for (let i = 0; i <= lastIndex; i++) {
+//   if (i !== lastIndex) {
+//     newArr[i] = arr[i];
+//   }
+// }
+// console.log(newArr);
+
+// arr.shift();
+
+// for (item of arr) {
+//   console.log(item);
+// }
+
+// map, filter, reduce, concat, slice, splice.
