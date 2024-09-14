6da0fc06571aaed3b4db08c7a611cb70fc4ad46f
wolfiee42 <saifalislam2022@gmail.com>
2024-09-14 16:36:27 +0600

    practicing concat method in imparative way, in 2 way

--- DSA/codevolutionDataStructure/Data/Array.js
+++ DSA/codevolutionDataStructure/Data/Array.js
@@ -60,16 +60,48 @@ let arr = [1, 2, 3, "Saif"];
 
 let arr2 = [1, 2, 3, 4];
 
+// reduce
 // const totalPrice = arr2.reduce((accumulator, arr2) => {
 //   return (accumulator += arr2);
 // }, 0);
 
-let sum = 0;
-for (let i = 0; i < arr2.length; i++) {
-  sum += arr2[i];
-}
+// let sum = 0;
+// for (let i = 0; i < arr2.length; i++) {
+//   sum += arr2[i];
+// }
 
 // console.log(totalPrice);
-console.log(sum);
+// console.log(sum);
 
-//  reduce, concat, slice, splice.
+const arr1 = ["Cecilie", "Lone"];
+const arr24 = ["Emil", "Tobias", "Linus"];
+const arr3 = ["Robin"];
+const children = arr1.concat(arr24, arr3);
+console.log(children);
+
+let newArr = [];
+let newArrInd = 0;
+
+newArr = [...arr1, ...arr24, ...arr3];
+console.log(newArr);
+
+let newArr2 = [];
+let newArrInd2 = 0;
+
+for (let i = 0; i < arr1.length; i++) {
+  newArr2[newArrInd2] = arr1[i];
+  newArrInd2++;
+}
+
+for (let i = 0; i < arr24.length; i++) {
+  newArr2[newArrInd2] = arr24[i];
+  newArrInd2++;
+}
+for (let i = 0; i < arr3.length; i++) {
+  newArr2[newArrInd2] = arr3[i];
+  newArrInd2++;
+}
+
+console.log(newArr2);
+
+//   concat, slice, splice.
