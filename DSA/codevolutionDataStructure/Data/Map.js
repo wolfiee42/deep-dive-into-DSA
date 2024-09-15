258ca475e6cf174bc8004b9ae5455bce40b842d0
wolfiee42 <saifalislam2022@gmail.com>
2024-09-15 15:47:18 +0600

    understanding map data structure

---
+++ DSA/codevolutionDataStructure/Data/Map.js
@@ -0,0 +1,15 @@
+const map = new Map([
+  ["a", 1],
+  ["b", 2],
+]);
+
+map.set("c", 3);
+
+console.log(map.has("b"));
+map.delete("c");
+
+console.log(map.size);
+map.clear();
+for (const [key, value] of map) {
+  console.log(`${key}: ${value}`);
+}
