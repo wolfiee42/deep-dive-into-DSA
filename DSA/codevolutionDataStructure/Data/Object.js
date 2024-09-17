04071c9961da29a3ead425ce886b7d743f044516
wolfiee42 <saifalislam2022@gmail.com>
2024-09-17 16:26:37 +0600

    understanding the fundamental of object

--- DSA/codevolutionDataStructure/Data/Object.js
+++ DSA/codevolutionDataStructure/Data/Object.js
@@ -10,10 +10,22 @@ const obj = {
 obj.hobby = "football";
 delete obj["key-three"];
 
-console.log(obj.name);
-console.log(obj["age"]);
-console.log(obj["key-three"]);
-console.log(obj);
-obj.sayMyName();
+// console.log(obj.name);
+// console.log(obj["age"]);
+// console.log(obj["key-three"]);
+// console.log(obj);
+// obj.sayMyName();
 
 // Object.keys()  .values() .entries()
+
+let person = {
+  name: "Saif",
+  age: 25,
+  job: "Developer",
+  city: "New York",
+  skills: ["JavaScript", "HTML", "CSS"],
+};
+
+console.log(Object.keys(person)); // returns array of the keys.
+console.log(Object.values(person)); // returns array of the values.
+console.log(Object.entries(person)); // returns 2 dymensional array of the object.
