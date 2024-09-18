317deca5aa350f7127c956c7dc6919a7da615480
wolfiee42 <saifalislam2022@gmail.com>
2024-09-18 16:23:23 +0600

    created a stack database by myself, took gpt's suggstion in isFull().

--- DSA/codevolutionDataStructure/Data/stackDS.js
+++ DSA/codevolutionDataStructure/Data/stackDS.js
@@ -5,4 +5,57 @@
 // pop
 // peek
 // length
+// isEmpty
+// count
+// isfull
 // example: javascript runtime stack, History Stack, Undo/Redo Stack, etc.
+
+class Stack {
+  constructor(maxSize) {
+    this.items = [];
+    this.maxSize = maxSize;
+  }
+
+  push(element) {
+    if (this.items.length < this.maxSize) {
+      return this.items.push(element);
+    }
+    return `Array is full.`;
+  }
+
+  pop() {
+    return this.items.pop();
+  }
+
+  peek() {
+    const length = this.items.length;
+    return this.items[length - 1];
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  count() {
+    return this.items.length;
+  }
+
+  isFull() {
+    return this.items.length === this.maxSize;
+  }
+}
+
+const stack = new Stack(4);
+
+stack.push(10);
+stack.push(20);
+stack.push(30);
+stack.push(40);
+// console.log(stack.push(50));
+// stack.pop();
+console.log(stack.peek());
+console.log(stack.isEmpty());
+console.log(stack.count());
+console.log(stack.isFull());
+
+console.log(stack);
