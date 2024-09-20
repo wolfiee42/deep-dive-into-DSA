b15d76755394af4a9526c556325d8abb6848eb73
wolfiee42 <saifalislam2022@gmail.com>
2024-09-20 23:34:51 +0600

    understanding peek functionality for circular queue

--- DSA/codevolutionDataStructure/Data/circularQueue.js
+++ DSA/codevolutionDataStructure/Data/circularQueue.js
@@ -1,10 +1,10 @@
 class CircularQueue {
   constructor(capacity) {
     this.items = new Array(capacity);
-    this.capacity = capacity;
     this.currentLength = 0;
     this.rear = -1;
     this.front = -1;
+    this.capacity = capacity;
   }
 
   isFull() {
@@ -16,14 +16,13 @@ class CircularQueue {
   }
 
   enqueue(element) {
-    if (this.isFull()) {
-      return `Queue is full. Can not insert any items.`;
-    }
-    this.rear = (this.rear + 1) % this.capacity;
-    this.items[this.rear] = element;
-    this.currentLength += 1;
-    if (this.front === -1) {
-      this.front = this.rear;
+    if (!this.isFull()) {
+      this.rear = (this.rear + 1) % this.capacity;
+      this.items[this.rear] = element;
+      this.currentLength += 1;
+      if (this.front === -1) {
+        this.front = this.rear;
+      }
     }
   }
 
@@ -41,4 +40,28 @@ class CircularQueue {
     }
     return item;
   }
+
+  peek() {
+    if (this.isEmpty()) {
+      return `Queue is Empty`;
+    }
+    return this.items[this.front];
+  }
 }
+
+const queue = new CircularQueue(5);
+
+console.log(queue.isEmpty());
+queue.enqueue(10);
+queue.enqueue(20);
+queue.enqueue(30);
+queue.enqueue(40);
+queue.enqueue(50);
+console.log(queue.size());
+queue.print();
+console.log(queue.isFull());
+console.log(queue.dequeue());
+console.log(queue.peek());
+queue.print();
+queue.enqueue(60);
+queue.print();
