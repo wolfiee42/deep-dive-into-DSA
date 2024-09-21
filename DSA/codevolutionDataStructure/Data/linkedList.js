0953f1218e9c1fbced434d760c6758855e422f7c
wolfiee42 <saifalislam2022@gmail.com>
2024-09-21 16:05:22 +0600

    initialized class for linked list data structure

--- DSA/codevolutionDataStructure/Data/linkedList.js
+++ DSA/codevolutionDataStructure/Data/linkedList.js
@@ -1,6 +1,21 @@
-class node {
+class Node {
   constructor(value) {
     this.value = value;
     this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  getSize() {
+    return this.size;
   }
 }
