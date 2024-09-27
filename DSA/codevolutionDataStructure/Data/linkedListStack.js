2fc09504bb948746afbb828d0cb143313dd91cb7
wolfiee42 <saifalislam2022@gmail.com>
2024-09-27 10:05:39 +0600

    successfully create stack queue using linkedList

--- DSA/codevolutionDataStructure/Data/linkedListStack.js
+++ DSA/codevolutionDataStructure/Data/linkedListStack.js
@@ -14,7 +14,7 @@ class linkedListStack {
   }
 
   peek() {
-    return this.head.value;
+    return this.list.head.value;
   }
 
   isEmpty() {
@@ -22,10 +22,24 @@ class linkedListStack {
   }
 
   getSize() {
-    this.list.getSize();
+    return this.list.getSize();
   }
 
   print() {
     return this.list.print();
   }
 }
+
+const stack = new linkedListStack();
+console.log(stack.isEmpty());
+
+stack.push(10);
+stack.push(20);
+stack.push(30);
+console.log(stack.getSize());
+stack.print();
+
+stack.pop();
+stack.print();
+
+console.log(stack.peek());
