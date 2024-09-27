5361a08f1092d908c787bc44e1a754a79c1785d3
wolfiee42 <saifalislam2022@gmail.com>
2024-09-27 09:55:06 +0600

    creating initial set up for linkedlist

---
+++ DSA/codevolutionDataStructure/Data/linkedListStack.js
@@ -0,0 +1,19 @@
+const LinkedList = require("./linkedList02");
+
+class linkedListStack {
+  constructor() {
+    this.list = new LinkedList();
+  }
+
+  push(value) {}
+
+  pop() {}
+
+  peek() {}
+
+  isEmpty() {}
+
+  getSize() {}
+
+  print() {}
+}
