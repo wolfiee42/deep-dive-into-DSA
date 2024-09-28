dfbeef32f165b7465a50e84dd408d3af900161a2
wolfiee42 <saifalislam2022@gmail.com>
2024-09-28 16:09:44 +0600

    creating a hash method to hash key value pair

--- DSA/codevolutionDataStructure/Data/hashTable.js
+++ DSA/codevolutionDataStructure/Data/hashTable.js
@@ -3,6 +3,14 @@ class HashTable {
     this.table = new Array(size);
     this.size = this.size;
   }
+
+  hash(key) {
+    let total = 0;
+    for (let i = 0; i < key.length; i++) {
+      total += key.charCodeAt(i);
+    }
+    return total % this.size;
+  }
 }
 
 const table = new HashTable(50);
