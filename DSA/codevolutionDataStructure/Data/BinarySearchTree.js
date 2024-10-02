fb5c58bb04b259d851bd76fca5c6e1b582cc4eb3
wolfiee42 <saifalislam2022@gmail.com>
2024-10-02 15:30:10 +0600

    insert & insertNode implementation in bst

--- DSA/codevolutionDataStructure/Data/BinarySearchTree.js
+++ DSA/codevolutionDataStructure/Data/BinarySearchTree.js
@@ -14,7 +14,37 @@ class BinarySearchTree {
   isEmpty() {
     return this.root === null;
   }
+
+  insert(value) {
+    const newNode = new Node(value);
+
+    if (this.isEmpty()) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+
+  insertNode(root, newNode) {
+    if (newNode.value < root.value) {
+      if (root.left === null) {
+        this.left = newNode;
+      } else {
+        this.insertNode(root.left, newNode);
+      }
+    } else {
+      if (root.right === null) {
+        this.right = newNode;
+      } else {
+        this.insertNode(root.right, newNode);
+      }
+    }
+  }
 }
 
 const bst = new BinarySearchTree();
 console.log("Bst is empty?", bst.isEmpty());
+
+bst.insert(10);
+bst.insert(5);
+bst.insert(15);
