e784a2c3f1fca237ed573942f5f0c898bb6a3439
wolfiee42 <saifalislam2022@gmail.com>
2024-10-05 17:15:45 +0600

    implementing delete operator in bst

--- DSA/codevolutionDataStructure/Data/BinarySearchTree.js
+++ DSA/codevolutionDataStructure/Data/BinarySearchTree.js
@@ -109,6 +109,33 @@ class BinarySearchTree {
       return this.max(root.right);
     }
   }
+
+  delete(value) {
+    this.root = this.deleteNode(this.root, value);
+  }
+
+  deleteNode(root, value) {
+    if (root === null) {
+      return root;
+    }
+    if (value < root.value) {
+      root.left = this.deleteNode(root.left, value);
+    } else if (value > root.value) {
+      root.right = this.deleteNode(root.right, value);
+    } else {
+      if (!root.left && !root.right) {
+        return null;
+      }
+      if (!root.left) {
+        return root.right;
+      } else if (!root.right) {
+        return root.left;
+      }
+      root.value = this.min(root.right);
+      root.right = this.deleteNode(root.right, root.value);
+    }
+    return root;
+  }
 }
 
 const bst = new BinarySearchTree();
@@ -118,17 +145,20 @@ bst.insert(10);
 bst.insert(5);
 bst.insert(15);
 bst.insert(3);
-bst.insert(7);
+// bst.insert(7);
 
-console.log(bst.search(bst.root, 10));
-console.log(bst.search(bst.root, 5));
-console.log(bst.search(bst.root, 25)); // false.
-console.log(bst.search(bst.root, 15));
+// console.log(bst.search(bst.root, 10));
+// console.log(bst.search(bst.root, 5));
+// console.log(bst.search(bst.root, 25)); // false.
+// console.log(bst.search(bst.root, 15));
 
 // bst.preOrder(bst.root);
 // bst.inOrder(bst.root);
 // bst.postOrder(bst.root);
 // bst.levelOrder();
 
-console.log(bst.min(bst.root));
-console.log(bst.max(bst.root));
+// console.log(bst.min(bst.root));
+// console.log(bst.max(bst.root));
+
+bst.delete(10);
+bst.levelOrder();
